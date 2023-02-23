8ec4208f3cc86b6fac1f2aafb58d6805cd4ee7e5
unknown <Riyanagwl@gmail.com>
2023-02-23 18:49:55 +0300

    add fuzz class

--- solver.rb
+++ solver.rb
@@ -10,3 +10,17 @@ class Solver
     str.reverse
   end
 end
+
+public
+
+def fizzbuzz(n)
+  if (n % 3).zero? && (n % 5).zero?
+    'fizzbuzz'
+  elsif (n % 3).zero?
+    'fizz'
+  elsif (n % 5).zero?
+    'buzz'
+  else
+    n.to_s
+  end
+end
