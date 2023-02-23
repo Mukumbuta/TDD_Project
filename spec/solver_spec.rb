f72f39d96c57809f5e50e48c8bb477b4de70f0b3
AhmadiGu <ahmadigulalai76@gmail.com>
2023-02-23 20:51:28 +0500

    Just did some changes

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -28,26 +28,26 @@ RSpec.describe Solver do
       expect(Solver.new.reverse('dad')).to eq('dad')
     end
   end
-  # describe '#fizzbuzz' do
-  #   it 'returns "fizz" when given a number divisible by 3' do
-  #     expect(Solver.new.fizzbuzz(3)).to eq('fizz')
-  #     expect(Solver.new.fizzbuzz(9)).to eq('fizz')
-  #     expect(Solver.new.fizzbuzz(12)).to eq('fizz')
-  #   end
-  #   it 'returns "buzz" when given a number divisible by 5' do
-  #     expect(Solver.new.fizzbuzz(5)).to eq('buzz')
-  #     expect(Solver.new.fizzbuzz(10)).to eq('buzz')
-  #     expect(Solver.new.fizzbuzz(20)).to eq('buzz')
-  #   end
-  #   it 'returns "fizzbuzz" when given a number divisible by 3 and 5' do
-  #     expect(Solver.new.fizzbuzz(15)).to eq('fizzbuzz')
-  #     expect(Solver.new.fizzbuzz(30)).to eq('fizzbuzz')
-  #     expect(Solver.new.fizzbuzz(45)).to eq('fizzbuzz')
-  #   end
-  #   it 'returns the number as a string when not divisible by 3 or 5' do
-  #     expect(Solver.new.fizzbuzz(7)).to eq('7')
-  #     expect(Solver.new.fizzbuzz(13)).to eq('13')
-  #     expect(Solver.new.fizzbuzz(22)).to eq('22')
-  #   end
-  # end
+  describe '#fizzbuzz' do
+    it 'returns "fizz" when given a number divisible by 3' do
+      expect(Solver.new.fizzbuzz(3)).to eq('fizz')
+      expect(Solver.new.fizzbuzz(9)).to eq('fizz')
+      expect(Solver.new.fizzbuzz(12)).to eq('fizz')
+    end
+    it 'returns "buzz" when given a number divisible by 5' do
+      expect(Solver.new.fizzbuzz(5)).to eq('buzz')
+      expect(Solver.new.fizzbuzz(10)).to eq('buzz')
+      expect(Solver.new.fizzbuzz(20)).to eq('buzz')
+    end
+    it 'returns "fizzbuzz" when given a number divisible by 3 and 5' do
+      expect(Solver.new.fizzbuzz(15)).to eq('fizzbuzz')
+      expect(Solver.new.fizzbuzz(30)).to eq('fizzbuzz')
+      expect(Solver.new.fizzbuzz(45)).to eq('fizzbuzz')
+    end
+    it 'returns the number as a string when not divisible by 3 or 5' do
+      expect(Solver.new.fizzbuzz(7)).to eq('7')
+      expect(Solver.new.fizzbuzz(13)).to eq('13')
+      expect(Solver.new.fizzbuzz(22)).to eq('22')
+    end
+  end
 end
